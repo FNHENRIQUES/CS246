a7eb7a299ffa1905c0a25e5bddbd776ef5245e8b
fnhenriques <nandoje@hotmail.com>
2020-02-08 09:06:58 -0300

    newf

---
+++ HelloFromFernando/src/henriques/HelloFromFernando.java
@@ -0,0 +1,10 @@
+package henriques;
+
+public class HelloFromFernando {
+    public static void main(String[] args){
+
+        System.out.println("Hello from Fernando!");
+        System.out.println("Wellcome to this repository, you can write a code here:");
+	System.out.println("Modifying bu bash");
+    }
+}
---
+++ HelloWorldApp/app/src/main/java/henriques/helloworldapp/MainActivity.java
@@ -0,0 +1,20 @@
+package henriques.helloworldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sayHello(View view){
+       TextView tv = findViewById(R.id.message);
+       tv.setText("Hi People!");
+
+    }
+}
